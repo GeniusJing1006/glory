c3ee45570cf132ec0f50ca107d9a119d1d01d839
zhaoyj1006 <976395188@qq.com>
2016-12-19 09:47:22 +0800

    push

--- public/js/component/component.js
+++ public/js/component/component.js
@@ -1,5 +1,5 @@
 var $radioList = $('.radio_box .radio_block');
-$radioList.on('tap',function(){
+$radioList.on('click',function(){
     //console.log($(this).index())
     var $self = $(this);
     if(!$self.hasClass('selected')){
@@ -7,7 +7,7 @@ $radioList.on('tap',function(){
         $self.addClass('selected');
     }
 });
-
+/** 
 var $selectTitle = $('.select-title');
 var $selectList = $('.select-list')
 $selectTitle.on('tap',function(){
@@ -20,5 +20,20 @@ $selectList.find('.select-li').on('tap',function(){
     var val = $self.data('val');
     //console.log(text.val);
     $parent.siblings('.select-title').text(text).data('val',val);
+    $parent.hide();
+})
+*/
+
+var $resTitle = $('.res_list');
+var $resList = $('.resource_list');
+$resTitle.on('click',function(){
+    $(this).siblings('.resource_list').toggle();
+});
+$resList.find('resource_li').on('click',function(){
+    var $self = $(this);
+    var $parent = $self.parent('.resource_list');
+    var text = $self.text();
+    var val = $self.data('val');
+    $parent.siblings('res_list').text(text).data('val',val);
     $parent.hide();
 })
