76993c6089810f2d76215f074ee5cc257ea657ca
zhaoyj1006 <976395188@qq.com>
2016-12-20 11:38:56 +0800

    github 远程仓库提交

--- public/js/component/component.js
+++ public/js/component/component.js
@@ -1,5 +1,5 @@
 var $radioList = $('.radio_box .radio_block');
-$radioList.on('click',function(){
+$radioList.on('tap',function(){
     //console.log($(this).index())
     var $self = $(this);
     if(!$self.hasClass('selected')){
@@ -7,10 +7,44 @@ $radioList.on('click',function(){
         $self.addClass('selected');
     }
 });
-/** 
+
+var $radioLi = $('.res_radio .radio_block');
+$radioLi.on('tap',function(){
+    var $self = $(this);
+    if(!$self.hasClass('selected')){
+        $radioLi.removeClass('selected');
+        $self.addClass('selected');
+    }else{
+        $self.removeClass('selected')
+    }
+});
+
+var $radioLi = $('.res_radio2 .radio_block');
+$radioLi.on('tap',function(){
+    var $self = $(this);
+    if(!$self.hasClass('selected')){
+        $radioLi.removeClass('selected');
+        $self.addClass('selected');
+    }else{
+        $self.removeClass('selected')
+    }
+});
+
+var $radioLi = $('.res_radio3 .radio_block');
+$radioLi.on('tap',function(){
+    var $self = $(this);
+    if(!$self.hasClass('selected')){
+        $radioLi.removeClass('selected');
+        $self.addClass('selected');
+    }else{
+        $self.removeClass('selected')
+    }
+});
+
+/**
 var $selectTitle = $('.select-title');
 var $selectList = $('.select-list')
-$selectTitle.on('tap',function(){
+$selectTitle.on('click',function(){
     $(this).siblings('.select-list').toggle();
 });
 $selectList.find('.select-li').on('tap',function(){
@@ -24,9 +58,10 @@ $selectList.find('.select-li').on('tap',function(){
 })
 */
 
+
 var $resTitle = $('.res_list');
 var $resList = $('.resource_list');
-$resTitle.on('click',function(){
+$resTitle.on('tap',function(){
     $(this).siblings('.resource_list').toggle();
 });
 $resList.find('resource_li').on('click',function(){
---
+++ public/js/login/login.js
@@ -0,0 +1,9 @@
+$('.bt-reg').on('tap',function(){
+    location.href = './register.html'
+})
+$('.bt-passwd').on('tap',function(){
+    location.href = './forget.html'
+})
+$('.icon-back').on('tap',function(){
+    location.href = '../loading.html'
+})
